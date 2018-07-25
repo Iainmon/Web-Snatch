b693563fef924ca03d4894f79a683337fd116716
Iain Moncrief <iainmoncrief@gmail.com>
2018-07-25 11:16:59 -1000

    new

--- Snatch.cr
+++ Snatch.cr
@@ -8,6 +8,18 @@ ENDC = "\033[0m"
 BOLD = "\033[1m"
 UNDERLINE = "\033[4m"
 
+#Functions
+def run_cmd(cmd)
+    stdout = IO::Memory.new
+    stderr = IO::Memory.new
+    status = Process.run(cmd, args: "", output: stdout, error: stderr)
+    if status.success?
+      {status.exit_code, stdout.to_s}
+    else
+      {status.exit_code, stderr.to_s}
+    end
+  end
+
 #Dialogs
 puts "#{OKGREEN}Hello! Enter a domain to snatch from!#{ENDC}"
 domain = gets
@@ -20,4 +32,11 @@ path = "/" if path
 #     domainPath = "#{domain}/#{path}"
 # end
 domainPath = "#{domain}/#{path}"
-puts "#{OKGREEN}Where would you like to put the files from #{OKBLUE + BOLD}#{domain}#{ENDC}#{WARNING}#{path}#{ENDC} #{OKGREEN}?#{ENDC}"
+puts "#{OKGREEN}Where would you like to put the files from #{OKBLUE + BOLD}#{domain}#{ENDC}#{WARNING}#{path}#{ENDC} #{OKGREEN}?#{ENDC} #{WARNING}(Default is '/')#{ENDC}"
+
+userPath = gets
+userPath = "" if userPath
+
+cmd = "cd #{userPath}; wget --recursive --page-requisites --adjust-extension --span-hosts --convert-links --restrict-file-names=windows --domains #{domain} --no-parent #{domainPath} "
+status, output = run_cmd(cmd)
+puts "#{FAIL + BOLD}WOOPS!?! Something sent wrong!#{ENDC} Make sure you have wget installed, connected to the internet, the folder exists, and the domain is correct." unless status == 0
